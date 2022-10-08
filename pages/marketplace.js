089c621cbc9e8ae399fd87767b782db25e0368c2
Elton <57287843+eltontay@users.noreply.github.com>
2022-10-08 14:53:32 +0800

    added basic functionalities

--- pages/marketplace.js
+++ pages/marketplace.js
@@ -1,13 +1,21 @@
-import Image from 'next/image';
-import React, { Component, useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useUser } from '../context/user';
 import Navbar from '../components/navbar';
 
 function Marketplace() {
   const { state } = useUser();
-  const ticketsSale = useRef({});
   const [isInitialRender, setIsInitialRender] = useState(true);
+  const [tickets, setTickets] = useState([]);
+  const [purchase, setPurchase] = useState(false);
+  const [listId, setListId] = useState(0);
+  const [sellingPrice, setSellingPrice] = useState(0);
   useEffect(() => {
+    const purchase = async (arr) => {
+      setPurchase(true);
+      setListId(arr[0]);
+      setSellingPrice(arr[2]);
+    };
+
     const getTicketsOnSale = async () => {
       try {
         var s;
@@ -15,33 +23,44 @@ function Marketplace() {
           .totalSupply()
           .call({ from: state.address })
           .then((response) => {
-            console.log('supply is', response);
             s = response;
           });
-        console.log(s);
-        var ticketsOnSale = {};
-
+        var array = [];
         for (var i = 1; i <= s; i++) {
           const sale = await state.fnft.methods
             .getForSale(i)
-            .call({ from: state.address })
-            .then(console.log);
+            .call({ from: state.address });
           if (sale) {
-            const owner = await state.fnft.methods
-              .ownerOf(i)
-              .call({ from: state.address })
-              .then(console.log);
-            const sellingPrice = await state.fnft.methods
-              .getSellingPrice(i)
-              .call({ from: state.address })
-              .then(console.log);
-            ticketsOnSale[i] = {
-              address: owner,
-              sellingPrice: sellingPrice,
+            console.log('pass');
+            var renderData = async () => {
+              var sellingPrice;
+              await state.fnft.methods
+                .getSellingPrice(i)
+                .call({ from: state.address })
+                .then((response) => {
+                  sellingPrice = response;
+                });
+              console.log(sellingPrice);
+              return (
+                <tr key={i}>
+                  <td>{i}</td>
+                  <td>{sellingPrice}</td>
+                  <td>
+                    <button
+                      className="w-full uppercase border-light-blue border-2 inline-block text-sm bg-grey px-4 p-2 rounded-full font-semibold hover:bg-indigo-100"
+                      value={[i, sellingPrice]}
+                      onClick={(e) => purchase(e.target.value)}
+                    >
+                      Purchase
+                    </button>
+                  </td>
+                </tr>
+              );
             };
+            array.push(await renderData());
           }
         }
-        ticketsSale.current = ticketsOnSale;
+        setTickets(array);
       } catch (error) {
         console.log(error);
       }
@@ -50,43 +69,61 @@ function Marketplace() {
       setIsInitialRender(false);
       getTicketsOnSale();
     }
-  }, [state, isInitialRender]);
+  }, [state, isInitialRender, tickets]);
+
+  const purchaseTicket = async () => {
+    await state.fnft.methods
+      .adjustListing(listId, sellingPrice)
+      .send({
+        from: state.address,
+      })
+      .then(function (result) {
+        console.log(result);
+      });
+  };
 
   return (
     <div className="">
       <Navbar />
-      {/* <div classNameName="h-screen grid bg-gradient-to-tr from-indigo-800 to-indigo-500"> */}
-      <div className="h-screen bg-gradient-to-tr from-indigo-800 to-indigo-500  p-6">
-        <h2 className="text-2xl font-bold tracking-tight text-gray-900">
-          Secondary Sale
-        </h2>
-
-        <div className="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
-          <div className="group relative">
-            <div className="min-h-80 aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-md bg-gray-200 group-hover:opacity-75 lg:aspect-none lg:h-80">
-              {/* <Image
-                src="https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg"
-                alt="Front of men&#039;s Basic Tee in black."
-                className="h-full w-full object-cover object-center lg:h-full lg:w-full"
-              /> */}
-            </div>
-            <div className="mt-4 flex justify-between">
-              <div>
-                <h3 className="text-sm text-black text-bold">
-                  <a href="#">
-                    <span
-                      aria-hidden="true"
-                      className="absolute inset-0"
-                    ></span>
-                    Basic Tee
-                  </a>
-                </h3>
-              </div>
-              <p className="text-sm font-medium text-black"></p>
+      <div className="h-screen col-span-4 bg-gradient-to-tr from-indigo-800 to-indigo-500 flex justify-center">
+        <div className="h-auto flex flex-wrap content-center">
+          <div className="flex justify-center p-8">
+            <div className="block rounded-lg shadow-lg bg-white max-w-sm ">
+              <table className="table-auto border-separate border-spacing-4 ">
+                <thead>
+                  <tr>
+                    <th key="id">Ticket ID</th>
+                    <th key="sellingPrice">Selling Price</th>
+                    <th key="purchase">Purchase</th>
+                  </tr>
+                </thead>
+                <tbody>{tickets}</tbody>
+              </table>
+              {purchase ? (
+                <form className="flex w-full max-w-sm justify-center">
+                  <div className="flex items-center py-2">
+                    <button
+                      className="flex-shrink-0 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded"
+                      type="button"
+                      onClick={() => purchaseTicket()}
+                    >
+                      Confirm Purchase
+                    </button>
+                    <button
+                      className="flex-shrink-0 border-transparent border-4 text-teal-500 hover:text-teal-800 text-sm py-1 px-2 rounded"
+                      type="button"
+                      onClick={() => setPurchase(false)}
+                    >
+                      Cancel
+                    </button>
+                  </div>
+                </form>
+              ) : (
+                <div></div>
+              )}
             </div>
           </div>
         </div>
-        {/* </div> */}
       </div>
     </div>
   );
